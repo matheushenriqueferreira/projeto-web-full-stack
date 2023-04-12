5ad40199a528a6310786c11e90451e7b9fee0a41
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-04-12 19:34:30 -0300

    React router dom, Home path

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
