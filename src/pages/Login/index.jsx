d512b13334d904b42f807fa50b9a2ca96e792b5d
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-04-13 22:00:40 -0300

    Login component

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from '../../components/Navbar';
+
+
+const Login = () => {
+  return(
+    <>
+      <Navbar />
+      <main>
+        <h1>Login</h1>
+      </main>
+    </>
+  );
+}
+
+export default Login;
