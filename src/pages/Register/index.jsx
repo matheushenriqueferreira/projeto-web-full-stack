f2dedc0743e6ddff142bfc81c9304fdd13728661
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-04-13 22:00:05 -0300

    Register component

---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './index.css';
+import Navbar from "../../components/Navbar";
+
+const Register = () => {
+  return(
+    <>
+      <Navbar />
+      <main>
+        <h1>Register</h1>
+      </main>
+    </>
+  );
+}
+
+export default Register;
