f2603e3d73b5b8076be535358e6616298f71ff53
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-04-17 15:35:21 -0300

    CSS File removed and add className prop

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import './index.css'
 
-const Input = ({ type, placeholder, btnValue, value, setValue, btnClick }) => {
+const Input = ({ className, type, placeholder, btnValue, value, setValue, btnClick }) => {
   return(
     type === 'button' ?
-    <input className={type} type={type} value={btnValue} onClick={btnClick} />
+    <input className={className} type={type} value={btnValue} onClick={btnClick} />
     :
-    <input className={type} type={type} placeholder={placeholder} value={value} onChange={(e) => setValue(e.target.value)} />
+    <input className={className} type={type} placeholder={placeholder} value={value} onChange={(e) => setValue(e.target.value)} />
   );
 }
 
