3734f2b5c455ba6d43dad4dacd605ef3aab9b07c
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-04-30 14:23:33 -0300

    Add new props

--- frontend/src/components/Searchbar/index.jsx
+++ frontend/src/components/Searchbar/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import './index.css';
 
-const Searchbar = ({ inputSearchValue, setInputSearchValue, quantity, setQuantity, max }) => {
+const Searchbar = ({ searchInputValue, setSearchInputValue, quantity, setQuantity, max, clickButton, handleSearchInput }) => {
   return(
     <div className="searchMenu">
-      <input className={'searchInputTextStyle'} type={'text'} placeholder={'Buscar'} value={inputSearchValue} onChange={(evt) => setInputSearchValue(evt.target.value)} />
-      <button className={'searchInputBtnStyle'} type={'button'}>Buscar</button>
+      <input className={'searchInputTextStyle'} type={'text'} placeholder={'Buscar'} value={searchInputValue} onChange={(evt) => {setSearchInputValue(evt.target.value, handleSearchInput(evt.target.value))}} />
+      <button className={'searchInputBtnStyle'} type={'button'} onClick={() => clickButton()}>Buscar</button>
       <div className="rangeContainer">
-        <input className={'searchInputRangeStyle'} type={'range'} value={quantity} min={'1'} max={max} onChange={(e) => setQuantity(e.target.value)} />
+        <input className={'searchInputRangeStyle'} type={'range'} value={quantity} min={'0'} max={max} onChange={(e) => setQuantity(e.target.value)} />
         <p>{quantity}</p>
       </div>
     </div>
