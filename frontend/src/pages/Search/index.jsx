f86ce83f2f73588a27f574c70cfe9f4ed1ba511c
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-04-30 14:51:58 -0300

    handleSerachInput, handleSearchButton

--- frontend/src/pages/Search/index.jsx
+++ frontend/src/pages/Search/index.jsx
@@ -8,21 +8,49 @@ import Card from "../../components/Card";
 
 const Search = () => {
   const [ itemsQuantity, setItemsQuantity ] = useState('1');
-  const [ message, setMessage ] = useState('');
   const [ annotations, setAnnotations ] = useState([]);
+  const [ tempList, setTempList ] = useState([]);
+  const [ listMessage, setListMessage ] = useState('');
   const [ search, setSearch ] = useState('');
 
-  const filteredList = annotations.length > 0 ?
-    annotations.filter(annotation => annotation.textNote.includes(search))
-    : [];
+  const handleSearchInput = (evt) => {
+    if(evt === '') {
+      setAnnotations(tempList);
+      setItemsQuantity(tempList.length);
+      tempList.length === 0 ? setListMessage(`Nenhuma anotação cadastrada`) : setListMessage('');
+    }
+  }
+
+  const handleSearchButton = () => {
+    axios.get(`http://localhost:3000/annotations/${search}`, {textNote: search})
+    .then((resp) => {
+      setAnnotations(resp.data.annotationExists);
+      setItemsQuantity(resp.data.annotationExists.length);
+      resp.data.annotationExists.length === 0 ? setListMessage(`Não foram encontradas anotações para o termo digitado: ${search}`) : setListMessage('');
+    })
+    .catch((error) => {
+      setAnnotations([]);
+      setItemsQuantity(0);
+      setListMessage(`Erro ao realizar busca`);
+    });
+  }
 
   useEffect(() => {
     axios.get('http://localhost:3000/annotations')
     .then((resp) => {
-      setAnnotations(resp.data.annotationExists)
+      setAnnotations(resp.data.annotationExists);
+      setTempList(resp.data.annotationExists);
+      setItemsQuantity((resp.data.annotationExists).length);
+      resp.data.annotationExists.length === 0 ? setListMessage(`Nenhuma anotação cadastrada`) : setListMessage('');
     })
     .catch((error) => {
-      setMessage(error);
+      switch(error.response.status) {
+        case 500:
+          setListMessage('Erro no servidor, não foi possível estabelecer uma conexão')
+        break;
+        default: setListMessage('Erro')
+        break;
+      }
     })
   }, []);
   
@@ -30,11 +58,13 @@ const Search = () => {
     <>
       <Navbar />
       <main>
-        <Searchbar inputSearchValue={search} setInputSearchValue={setSearch} quantity={itemsQuantity} setQuantity={setItemsQuantity} max={annotations.length} />
+      <Searchbar searchInputValue={search} setSearchInputValue={setSearch} quantity={itemsQuantity} setQuantity={setItemsQuantity} max={annotations.length} clickButton={handleSearchButton} handleSearchInput={handleSearchInput} />
         <article className="searchArticle paddingContent">
           <section className="cardSection">
             {
-              filteredList.slice(0, itemsQuantity).map((item, i) => <Card key={item._id} textNote={item.textNote} id={item.id} />)
+              annotations.length > 0 ? annotations.slice(0, itemsQuantity).map((item, i) => <Card key={item._id} textNote={item.textNote} />)
+              :
+              <p>{listMessage}</p>
             }
           </section>
         </article>
