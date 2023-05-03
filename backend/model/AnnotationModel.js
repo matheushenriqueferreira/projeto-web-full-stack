f4574b898595443b3fb9058199d21b0d685de341
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-05-03 14:47:10 -0300

    DB URI

--- backend/model/AnnotationModel.js
+++ backend/model/AnnotationModel.js
@@ -3,8 +3,7 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-//const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fltl8n2.mongodb.net/?retryWrites=true&w=majority`;
-const uri = process.env.DB_LOCAL_URI;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fltl8n2.mongodb.net/?retryWrites=true&w=majority`;
 const dbName = process.env.DB_NAME;
 
 export class AnnotationModel {
