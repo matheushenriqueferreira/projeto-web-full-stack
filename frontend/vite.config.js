a93882715814910558419ddfb899ce282cc8518b
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-06-15 22:10:18 -0300

    Vite HTTPS with SSL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { https: {
+    cert: fs.readFileSync('SSL/code.crt'),
+    key: fs.readFileSync('SSL/code.key')
+  }},
   plugins: [react()],
 })
